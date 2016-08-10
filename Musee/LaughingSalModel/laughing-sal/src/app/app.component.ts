50999e321d870c39961da2164b81be0fe02974e1
David Fisher <fisherds@rose-hulman.edu>
2016-08-09 23:22:03 -0400

    Tested the model objects for Sal Battle.  Seem to work.

--- Musee/LaughingSalModel/laughing-sal/src/app/app.component.ts
+++ Musee/LaughingSalModel/laughing-sal/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Battle } from "./battle.model";
+import { Battle, Weapon, SalBodyPart } from "./battle.model";
 
 @Component({
   moduleId: module.id,
@@ -11,14 +11,86 @@ export class AppComponent {
   title = 'Laughing Sal';
 
   constructor() {
-    this.testBattle1();
-
-
+    // this.testBattle1();
+    this.testPerfectBattle();
   }
 
   testBattle1() {
-    let battle1: Battle = new Battle();
-    console.log(`Fresh battle = ${battle1}`);
+    let battle: Battle = new Battle();
+    console.log(`Fresh battle: Selection = None, 6 remaining. Sal WAITING_FOR_SWORD_TO_THE_BODY at 100% health ${battle}`);
 
+    battle.selectWeapon(Weapon.LIGHTNING);
+    console.log(`Select lightning: Selection = LIGHTNING, 6 remaining. Sal WAITING_FOR_SWORD_TO_THE_BODY at 100% health  ${battle}`);
+
+    battle.attackSal(SalBodyPart.HANDS);
+    console.log(`Attached wrong location: Selection = None, 5 remaining. Sal MISTAKE_MADE at 100% health ${battle}`);
+
+    battle.attackSal(SalBodyPart.HANDS);
+    console.log(`Attached with no weapon: No change from prior state ${battle}`);
+
+    battle.selectWeapon(Weapon.LIGHTNING);
+    console.log(`Select lightning again: No change from prior state  ${battle}`);
+
+    battle.selectWeapon(Weapon.FIRE);
+    battle.attackSal(SalBodyPart.BODY);
+    console.log(`Attack FIRE: Selection = None, 4 remaining. Sal MISTAKE_MADE at 85% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.WATER);
+    battle.attackSal(SalBodyPart.HEAD);
+    console.log(`Attack WATER: Selection = None, 3 remaining. Sal MISTAKE_MADE at 70% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.ICE);
+    battle.attackSal(SalBodyPart.HEAD);
+    console.log(`Attack ICE: Selection = None, 2 remaining. Sal MISTAKE_MADE at 55% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.SWORD);
+    battle.attackSal(SalBodyPart.BODY);
+    console.log(`Attack SWORD: Selection = None, 1 remaining. Sal MISTAKE_MADE at 40% health  ${battle}`);
+
+    console.log(`Is battle still going ${battle.isBattleStillInProgress() ? "Yes" : "No"}`);
+    console.log(`Is Sal dead ${battle.isSalDead() ? "Yes" : "No"}`);
+
+    battle.selectWeapon(Weapon.MUD);
+    battle.attackSal(SalBodyPart.HANDS);
+    console.log(`Attack MUD: Selection = None, 0 remaining. Sal MISTAKE_MADE at 25% health  ${battle}`);
+
+    console.log(`Is battle still going ${battle.isBattleStillInProgress() ? "Yes" : "No"}`);
+    console.log(`Is Sal dead ${battle.isSalDead() ? "Yes" : "No"}`);
   }
+
+  testPerfectBattle() {
+    let battle: Battle = new Battle();
+    console.log(`Fresh battle: Selection = None, 6 remaining. Sal WAITING_FOR_SWORD_TO_THE_BODY at 100% health ${battle}`);
+
+    battle.selectWeapon(Weapon.SWORD);
+    battle.attackSal(SalBodyPart.BODY);
+    console.log(`Attack SWORD: Selection = None, 5 remaining. Sal WAITING_FOR_FIRE_TO_THE_BODY at 85% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.FIRE);
+    battle.attackSal(SalBodyPart.BODY);
+    console.log(`Attack FIRE: Selection = None, 4 remaining. Sal WAITING_FOR_ICE_TO_THE_HEAD at 70% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.ICE);
+    battle.attackSal(SalBodyPart.HEAD);
+    console.log(`Attack ICE: Selection = None, 3 remaining. Sal WAITING_FOR_LIGHTNING_TO_THE_HEAD at 55% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.LIGHTNING);
+    battle.attackSal(SalBodyPart.HEAD);
+    console.log(`Attached LIGHTNING: Selection = None, 2 remaining. Sal WAITING_FOR_MUD_TO_THE_HANDS at 40% health  ${battle}`);
+
+    battle.selectWeapon(Weapon.MUD);
+    battle.attackSal(SalBodyPart.HANDS);
+    console.log(`Attack MUD: Selection = None, 1 remaining. Sal WAITING_FOR_WATER_TO_THE_HEAD at 25% health  ${battle}`);
+
+    console.log(`Is battle still going ${battle.isBattleStillInProgress() ? "Yes" : "No"}`);
+    console.log(`Is Sal dead ${battle.isSalDead() ? "Yes" : "No"}`);
+
+    battle.selectWeapon(Weapon.WATER);
+    battle.attackSal(SalBodyPart.HEAD);
+    console.log(`Attack WATER: Selection = None, 0 remaining. Sal VICTORY at 10% health  ${battle}`);
+
+    console.log(`Is battle still going ${battle.isBattleStillInProgress() ? "Yes" : "No"}`);
+    console.log(`Is Sal dead ${battle.isSalDead() ? "Yes" : "No"}`);
+  }
+
 }
--- Musee/LaughingSalModel/laughing-sal/src/app/battle.model.ts
+++ Musee/LaughingSalModel/laughing-sal/src/app/battle.model.ts
@@ -1,22 +1,12 @@
-
-enum SalState {
-  WAITING_FOR_SWORD_TO_THE_BODY, // no armor
-  WAITING_FOR_FIRE_TO_THE_BODY, // icy heart
-  WAITING_FOR_ICE_TO_THE_HEAD, // hot head
-  WAITING_FOR_LIGHTNING_TO_THE_HEAD, // hear shocking news
-  WAITING_FOR_MUD_TO_THE_HANDS, // hands dirty
-  WAITING_FOR_WATER_TO_THE_HEAD, // makeup
-  VICTORY,
-  MISTAKE_MADE,
-}
-
-enum SalBodyPart {
+export enum SalBodyPart {
   HEAD,
   BODY,
   HANDS,
 }
 
-enum Weapon {
+// The order of this enum is used in a lot of places.  Be careful with changes.
+// Note, this happens to be the correct order the weapons are used in as well.
+export enum Weapon {
   SWORD,
   FIRE,
   ICE,
@@ -30,18 +20,54 @@ export class Battle {
   private sal: Sal;
 
   constructor() {
+    this.reset(); // I can never decide if I want a reset or if I want to force the other end to just make a new object.
+  }
+
+  // -------------- Actions ------------------
+  selectWeapon(weapon: Weapon) {
+    // Inform the Weapons object, then do very little as that doesn't effect the battle yet.
+    this.weapons.selectWeapon(weapon);
+  }
+
+  attackSal(bodyPart: SalBodyPart) {
+    if (!this.weapons.hasWeaponSelected) {
+      return;  // Silently do nothing.  Just pressed a body button at the wrong time.    
+    }
+    let useWeapon = this.weapons.useSelectedWeaponForAttack();
+    this.sal.attack(useWeapon, bodyPart);
+  }
+
+  reset() {
     this.weapons = new Weapons();
     this.sal = new Sal();
   }
 
-  selectWeapon(weapon: Weapon) {
-    // Inform the Weapons object, then do very little as that doesn't effect the battle yet.
-    this.weapons.selectWeapon(weapon);
-
+  // ---------------- Update view ---------------
+  isBattleStillInProgress(): boolean {
+    return this.weapons.hasWeaponsRemaining();
   }
 
-  attackSal(bodyPart: SalBodyPart) {
+  // Usually this is asked right after the question above.
+  isSalDead(): boolean {
+    return this.sal.isSalDead();
+  }
 
+  // Not really an indicator of death or state.  Just for the UI.
+  getSalHealth(): number {
+    return this.sal.health;
+  }
+
+  // Which button should be shown in the middle.
+  getSelectedWeapon(): Weapon {
+    if (this.weapons.hasWeaponSelected) {
+      return this.weapons.selectedWeapon;
+    }
+    return null;
+  }
+
+  // Which buttons should be shown at the bottom.
+  getSelectableWeaponsArray(): Array<boolean> {
+    return this.weapons.getWeaponsVisibleToSelect();
   }
 
   toString() {
@@ -52,25 +78,91 @@ export class Battle {
   }
 }
 
+// I would prefer this enum was WITHIN the Sal class, but the compiler didn't like that.
+enum SalState {
+  WAITING_FOR_SWORD_TO_THE_BODY, // no armor
+  WAITING_FOR_FIRE_TO_THE_BODY, // icy heart
+  WAITING_FOR_ICE_TO_THE_HEAD, // hot head
+  WAITING_FOR_LIGHTNING_TO_THE_HEAD, // hear shocking news
+  WAITING_FOR_MUD_TO_THE_HANDS, // hands dirty
+  WAITING_FOR_WATER_TO_THE_HEAD, // makeup
+  VICTORY,
+  MISTAKE_MADE,
+}
+
 // Private helper model object.  All calls go via the Battle.
 class Sal {
 
-  public salState: SalState = SalState.WAITING_FOR_SWORD_TO_THE_BODY;
+  public state: SalState;
+  public health: number;
 
-  reset() {
+  // Sal starts at 100% - 6 hits of 15% = 10% health (which you only get the last 10 for being in the right order)
+  DAMAGE_PER_HIT: number = 15;
 
+  // Convenience calculation to know when Sal is at her min.  The last 10% comes from the order being correct.
+  MIN_HEALTH: number = 10;
+
+  // Array of 6 items in the order of the Weapons enum, showing where each weapon should be used.
+  CORRECT_WEAPON_GUIDE: Array<SalBodyPart> = [SalBodyPart.BODY, SalBodyPart.BODY, SalBodyPart.HEAD, SalBodyPart.HEAD, SalBodyPart.HANDS, SalBodyPart.HEAD]
+
+  constructor() {
+    this.state = SalState.WAITING_FOR_SWORD_TO_THE_BODY;
+    this.health = 100;
   }
 
-  toString () {
-    return `state = ${SalState[this.salState]}`;
+  isSalDead() {
+    return this.state == SalState.VICTORY;
+  }
+
+  attack(weapon: Weapon, location: SalBodyPart): boolean {
+    if (location != this.CORRECT_WEAPON_GUIDE[weapon]) {
+      this.state = SalState.MISTAKE_MADE;
+      console.log("Attack to the wrong location. Ha ha!");
+      return false;
+    }
+    if (this.state == SalState.VICTORY) {
+      console.log("Sal just got hit when she was dead!");
+      return;
+    }
+    this.health -= this.DAMAGE_PER_HIT; // Sal loses health even if the order is wrong.
+    if (this.wasCorrectOrder(weapon)) {
+      this.state++; // Move along the success path towards VICTORY.
+    }
+    return true;
+  }
+
+  // Note, this method is called AFTER the weapon<->location is known to be correct.  Only worry about order here.
+  wasCorrectOrder(weapon: Weapon): boolean {
+    switch (this.state) {
+      case SalState.WAITING_FOR_SWORD_TO_THE_BODY:
+        return weapon == Weapon.SWORD;
+      case SalState.WAITING_FOR_FIRE_TO_THE_BODY:
+        return weapon == Weapon.FIRE;
+      case SalState.WAITING_FOR_ICE_TO_THE_HEAD:
+        return weapon == Weapon.ICE;
+      case SalState.WAITING_FOR_LIGHTNING_TO_THE_HEAD:
+        return weapon == Weapon.LIGHTNING;
+      case SalState.WAITING_FOR_MUD_TO_THE_HANDS:
+        return weapon == Weapon.MUD;
+      case SalState.WAITING_FOR_WATER_TO_THE_HEAD:
+        return weapon == Weapon.WATER;
+      case SalState.VICTORY:
+        console.log("Checked order at unexpected time!");
+      case SalState.MISTAKE_MADE:
+        return false;
+    }
+  }
+
+  toString() {
+    return `state = ${SalState[this.state]}   health = ${this.health}%`;
   }
 }
 
 // Private helper model object.  All calls go via the Battle.
 class Weapons {
 
-  private selectedWeapon: Weapon; // or null
-  private hasWeaponSelected: boolean; // It's a pain in JavaScript to be sure null isn't == 0, so just avoiding the headache of distinguishin between Weapon.SWORD and null.
+  public selectedWeapon: Weapon; // or null
+  public hasWeaponSelected: boolean; // It's a pain in JavaScript to be sure null isn't == 0, so just avoiding the headache of distinguishin between Weapon.SWORD and null.
   private remainingWeapons: Array<boolean> = new Array<boolean>();  // Array of 6 booleans in the order of the Weapon enum
 
   constructor() {
@@ -78,15 +170,20 @@ class Weapons {
     this.selectedWeapon = null;
     this.remainingWeapons = Array(6).fill(true);
   }
-  
+
   selectWeapon(weapon: Weapon) {
+    if (!this.remainingWeapons[weapon]) {
+      console.log("selectWeapon was called for a weapon that was not remaining!");
+      return;
+    }
     this.hasWeaponSelected = true;
     this.selectedWeapon = weapon;
   }
 
-  attachWithSelectedWeapon(): Weapon {
+  // Updates the weapons object and return the selected weapon that is being used for this attack.
+  useSelectedWeaponForAttack(): Weapon {
     if (!this.hasWeaponSelected) {
-      console.log("attachWithSelectedWeapon was called when no weapon was selected!!!");
+      console.log("attachWithSelectedWeapon was called when no weapon was selected!");
       return null;
     }
     let weaponToReturn = this.selectedWeapon;
@@ -112,7 +209,7 @@ class Weapons {
 
   getNumWeaponsRemaining(): number {
     var numRemaining: number = 0;
-    for (let isRemaining in this.remainingWeapons) {
+    for (let isRemaining of this.remainingWeapons) {
       if (isRemaining) {
         numRemaining++;
       }
